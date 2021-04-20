52d6636bed17482f4ba5739beab560a54cacfc75
Nur Uddin Shohan <nuruddin.shohan@yahoo.com>
2021-04-20 13:25:35 +0600

    Newsfeed component api_call

--- src/components/newsfeed.js
+++ src/components/newsfeed.js
@@ -3,13 +3,41 @@ import './newsfeed.css'
 import Post from './post'
 
 export default class Newsfeed extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            data : []
+        }
+    }
+    fetchApi(){
+        console.log("api called")
+        let url = "http://localhost:3030/testposts"
+        fetch(url)  
+            .then(response => response.json())
+            .then(json=>{
+                this.setState({
+                    data : json
+                })
+                //console.log(this.state.data)
+            })
+
+    }
+    componentDidMount(){
+        this.fetchApi()
+    }
     render(){
+        let data = this.state.data;
         return(
             <div className="newsfeed col">
                 <h2 className="newsfeed-heading">News Feed</h2>
-                <Post />
+                {
+                    data.map(ele=>{ 
+                        return <Post data = {ele} />
+                    })
+                }
+                
+               
 
-                <Post />
             </div>
         )
     }
--- src/components/post.js
+++ src/components/post.js
@@ -1,20 +1,20 @@
 import './post.css'
 
 
-export default function Post(){
+export default function Post(props){
     return(
         <div className="post">
             <div className="user">
                 <div className="img"></div>
                 <div>
-                    <p  className="user-name">Nur Uddin Shohan</p>
-                    <p className="upload-time">3rd Apr,21</p>
+                    <p  className="user-name">{props.data.author}</p>
+                    <p className="upload-time">{props.data.uptime}</p>
                 </div>
             </div>
             <div className="text">
                 <div className="text-col" >
-                    <h3>“The sky was cloudless and of a deep dark blue."</h3>
-                    <p>Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.</p>
+                    <h3>{props.data.title}</h3>
+                    <p>{props.data.des}</p>
                 </div>
                 <div className="cover-img"> </div>
             </div>
